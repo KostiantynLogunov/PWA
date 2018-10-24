98d76e84fb70ecce098f3e3bd39146ef09a8f127
KostiantynLogunov <777koc@gmail.com>
2018-10-24 13:41:07 +0300

    add icon

--- src/components/my-services/MyServices.vue
+++ src/components/my-services/MyServices.vue
@@ -5,6 +5,11 @@
             <span>You deleted service successfully!</span>
             <md-button class="md-accent" @click="flagDeleteService = false">Close</md-button>
         </md-snackbar>
+        <!--SNACKBAR--><!--SNACKBAR-->
+        <md-snackbar :md-persistent="true" :md-position="position" :md-duration="duration" :md-active.sync="flagCreateService" md-persistent>
+            <span>You created service successfully!</span>
+            <md-button class="md-accent" @click="flagCreateService = false">Close</md-button>
+        </md-snackbar>
         <!--SNACKBAR-->
 
         <md-progress-spinner :md-diameter="30" :md-stroke="3" md-mode="indeterminate" v-if="pandingResponseServer"></md-progress-spinner>
@@ -16,7 +21,7 @@
             <md-table v-model="my_services" md-sort="name" md-sort-order="asc" md-card >
                 <md-table-toolbar>
                     <h1 class="md-title">My Helping Services</h1>
-                    <button class="btn btn-success btn-sm">Add New Service</button>
+                    <button class="btn btn-success btn-sm" @click="onCreateFormService">Add New Service</button>
                 </md-table-toolbar>
 
                 <md-table-row slot="md-table-row" slot-scope="{ item }">
@@ -37,6 +42,7 @@
                         <button class="btn btn-danger btn-sm"
                                 @click="deleteItem(item.id)"><i class="fas fa-trash-alt"></i></button>
                         <button class="btn btn-primary btn-sm">Booking</button>
+                        <md-progress-bar md-mode="indeterminate" v-if="deletingService == item.id" />
                     </md-table-cell>
                 </md-table-row>
             </md-table>
@@ -111,6 +117,99 @@
                 </form>
             </div>
         </md-dialog>
+
+        <md-dialog :md-active.sync="creatingService">
+            <md-dialog-title>Create Service</md-dialog-title>
+            <div md-dynamic-height>
+                <form md-dynamic-height novalidate class="md-layout" @submit.prevent="CreateService()">
+                    <md-card class="md-layout-item md-size-100 md-small-size-100">
+
+                        <md-card-content>
+                            <div class="md-layout md-gutter">
+                                <div class="md-layout-item md-small-size-100">
+                                        <md-checkbox v-model="form_create_service.active"
+                                                     :disabled="processingService">Active</md-checkbox>
+                                    <br>
+                                        <md-radio v-model="form_create_service.type" value="provide">Provide service
+                                            <small>(Default)</small></md-radio>
+                                        <md-radio v-model="form_create_service.type" value="request"
+                                                  class="md-primary">Request service</md-radio>
+
+                                    <md-field>
+                                        <label>Name</label>
+                                        <md-input v-model="form_create_service.name"
+                                                  :disabled="processingService" />
+                                    </md-field>
+
+                                    <md-field>
+                                        <label>Description</label>
+                                        <md-textarea v-model="form_create_service.description" :disabled="processingService" />
+                                    </md-field>
+
+                                    <md-field>
+                                        <label>Price(optional)</label>
+                                        <md-input v-model="form_create_service.price"
+                                                  :disabled="processingService" />
+                                    </md-field>
+
+                                    <div class="md-layout-item md-small-size-100">
+                                        <md-field>
+                                            <label>Target Group</label>
+                                            <md-select name="event" v-model="form_create_service.group_id" md-dense
+                                                       placeholder="Please select target group"
+                                                       :disabled="processingService">
+                                                <div v-if="my_groups.length">
+                                                    <div v-for="group in my_groups">
+                                                        <md-option :value="group.id">
+                                                            {{ group.name }}
+                                                        </md-option>
+                                                    </div>
+                                                </div>
+                                                <div v-else>
+                                                    <md-option  disabled>
+                                                        Not any groups
+                                                    </md-option>
+                                                </div>
+                                            </md-select>
+                                        </md-field>
+                                    </div>
+
+
+                                    <br>
+                                </div>
+                                <br>
+                            </div>
+
+                        </md-card-content>
+
+                        <md-progress-bar md-mode="indeterminate" v-if="processingService" />
+
+                        <md-card-actions>
+                            <md-dialog-actions>
+                                <md-button :disabled="processingService" class="md-primary" @click="CancelCreateService">
+                                    Close</md-button>
+                                <md-button :disabled="processingService"
+                                           type="submit"
+                                           class="md-accent md-raised"
+                                >Update</md-button>
+                            </md-dialog-actions>
+                        </md-card-actions>
+
+                        <div class="deleteErrors" v-if="storeErrors">
+                            <p v-for="error in storeErrors">{{ error }}</p>
+                        </div>
+
+                        <div class="errors" v-if="createErrors">
+                            <ul>
+                                <li v-for="(fieldsError, fieldName) in createErrors" :key="fieldName">
+                                    {{ fieldsError.join('\n') }}
+                                </li>
+                            </ul>
+                        </div>
+                    </md-card>
+                </form>
+            </div>
+        </md-dialog>
     </div>
 </template>
 
@@ -123,12 +222,15 @@
         data(){
             return {
                 my_services: [],
+                my_groups: [],
+
                 pandingResponseServer: false,
 
                 position: 'left',
                 duration: 4000,
                 flagDeleteService: false,
 
+                deletingService: false,
                 editingService: false,
                 processingService: false,
                 form_edit_service:{
@@ -138,7 +240,21 @@
                     description: '',
                     price: '',
                     group: '',
-                }
+                },
+
+                flagCreateService: false,
+                creatingService: false,
+                storeErrors: [],
+                createErrors: [],
+                form_create_service:{
+                    active: '',
+                    type: 'provide',
+                    name: '',
+                    description: '',
+                    price: null,
+                    group_id: '',
+                    business_hours: null,
+                },
             }
         },
 
@@ -147,6 +263,106 @@
         },
 
         methods: {
+            getCreateConstraints(){
+                return {
+                    active: {
+                        presence: true,
+                    },
+                    type: {
+                        presence: true,
+                        length: {
+                            minimum: 8,
+                            message: 'Must be at least 3 characters long'
+                        }
+                    },
+                    name: {
+                        presence: true,
+                        length: {
+                            minimum: 3,
+                            message: 'Must be at least 3 characters long'
+                        }
+                    },
+                    description: {
+                        presence: true,
+                        length: {
+                            minimum: 5,
+                            message: 'Must be at least 5 characters long'
+                        }
+                    },
+                    group_id: {
+                        presence: true,
+                        numericality: {
+                            onlyInteger: true,
+                            greaterThanOrEqualTo: 0,
+                            message: 'Please select target group'
+                        }
+                    },
+                }
+            },
+
+            CreateService(){
+                this.createErrors = null;
+
+                const constraints = this.getCreateConstraints();
+
+                const errors = validate(this.form_create_item, constraints);
+
+                if (errors) {
+                    this.createErrors = errors;
+                    return ;
+                }
+                this.processingService = true;
+                let data = {
+                    name: this.htmlEntities(this.form_create_item.name),
+                    group_id: this.htmlEntities(this.form_create_item.group_id),
+                    description: this.htmlEntities(this.form_create_item.description),
+                    is_active: this.form_create_item.is_active ? 'on' : null,
+                };
+                // send to api this.form_create_item
+                axios.post(config.apiUrl + '/my_sharingitems', data, {
+                    headers: {
+                        "Authorization": `Bearer ${this.$store.getters.currentUser.token}`
+                    }
+                })
+                    .then((response) => {
+                        this.flagCreateService = true;
+                        this.getHelpingServices();
+                        this.processingService = false;
+                        this.clearCreateFormService();
+                    })
+                    .catch((err) => {
+                        /*let data_errors = [];
+                        data_errors.push(err.message);
+                        data_errors.push(err.response.data.message);
+                        this.errors = data_errors;
+                        console.log(this.errors);*/
+                        console.log(err);
+                    })
+                    .finally(() => {
+                        this.processingItem = false;
+                    });
+            },
+
+            CancelCreateService(){
+                this.creatingService = false;
+                this.clearCreateFormService();
+            },
+
+            onCreateFormService()
+            {
+                this.storeErrors = null;
+                this.createErrors = null;
+                this.creatingService = true;
+            },
+
+            clearCreateFormService(){
+                this.form_create_service.is_active = false;
+                this.form_create_service.name = '';
+                this.form_create_service.group_id = '';
+                this.form_create_service.description = '';
+            },
+
+
             onEditForm(service) {
                 this.editingService = true;
             },
@@ -156,18 +372,30 @@
 
             CancelEditingService(){
                 this.editingService = false;
-                console.log('canceling...');
+            },
+
+            findWithAttr(array, attr, value) {
+                for(var i = 0; i < array.length; i += 1) {
+                    if(array[i][attr] === value) {
+                        return i;
+                    }
+                }
+                return -1;
             },
 
             deleteItem(service_id) {
                 if ( !confirm('Are you sure you want to delete service? ') ) return;
+
+                this.deletingService = service_id;
+
                 axios.delete(config.apiUrl + '/my_services/' + service_id, {
                     headers: {
                         "Authorization": `Bearer ${this.$store.getters.currentUser.token}`
                     }
                 })
                     .then((response) => {
-                        this.getHelpingServices();
+                        let index = this.findWithAttr(this.my_services, 'id', service_id);
+                        this.$delete(this.my_services, index);
                         this.flagDeleteService = true;
                     })
                     .catch((err) => {
@@ -179,7 +407,7 @@
             },
 
             getHelpingServices() {
-                this.my_services = [];
+                // this.my_services = [];
                 this.pandingResponseServer = true;
                 axios.get(config.apiUrl + '/my_services', {
                     headers: {
@@ -192,6 +420,7 @@
 
                         let index, i = 0;
                         let group_serices = response.data.my_services;
+                        this.my_groups = response.data.my_groups;
 
                         for (index = 0; index < group_serices.length; index++) {
 
@@ -224,7 +453,11 @@
                     .finally(() => {
                         this.pandingResponseServer = false;
                     });
-            }
+            },
+
+            htmlEntities(str) {
+                return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g,'&apos');
+            },
         },
     }
 </script>
@@ -232,5 +465,10 @@
 <style scoped>
     .md-table{
         overflow: auto;
+    }
+</style>
+<style>
+    .md-menu-content {
+        z-index: 10;
     }
 </style>
